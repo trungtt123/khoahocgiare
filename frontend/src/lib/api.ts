d19d208ccab935f015087d9036d2900f2fe51aec
unknown <trungtran33421@gmail.com>
2025-12-30 21:37:54 +0700

    update

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://103.82.26.223:3001';
-// const API_URL = 'https://khoahocgiare.top';
+// const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://103.82.26.223:3001';
+const API_URL = 'https://khoahocgiare.top';
 
 
 const api = axios.create({
